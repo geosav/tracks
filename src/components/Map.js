aff4c504fe6699a2f3c89c0913e6bce1d628bb21
Yurii Savchuk <yurii.savchuk@ukietech.com>
2020-03-24 22:34:26 +0200

    Added base map with static coordinates to Create Track Screen.

---
+++ src/components/Map.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
+
+const Map = () => {
+    let points = [];
+    for (let i = 0; i < 20; i++) {
+        if (i % 2 === 0) {
+            points.push({
+                latitude: 48.43056 + i * 0.001,
+                longitude: 25.33241 + i * 0.001,
+            });
+        } else {
+            points.push({
+                latitude: 48.43056 - i * 0.002,
+                longitude: 25.33241 - i * 0.001,
+            });
+        }
+    }
+
+    return (
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                latitude: 48.43056,
+                longitude: 25.33241,
+                latitudeDelta: 0.01,
+                longitudeDelta:0.01
+            }}
+        >
+            <Polyline coordinates={points} />
+        </MapView>
+    );
+};
+
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+});
+
+export default Map;
